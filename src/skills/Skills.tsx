3626d31349428d7749fc851423b4f742d3d3f0aa
FrontendTerminator <mikutishvili.koba@gmail.com>
2021-04-24 00:49:47 +0300

    test deploy

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -5,28 +5,31 @@ import {Title} from "../common/components/title/Title";
 import {faHtml5} from '@fortawesome/free-brands-svg-icons';
 import {faReact} from '@fortawesome/free-brands-svg-icons';
 import {faJs} from '@fortawesome/free-brands-svg-icons';
+
 const Fade = require('react-reveal/Fade')
 
 export function Skills() {
 
     return (
         <div id={"skills"} className={style.skillsBlock}>
-            <div className={style.skillsContainer}>
-                <Title text={'My skills'}/>
-                <Fade bottom>
-                    <div className={style.skills}>
-                        <Skill title={"JS"}
-                               icon={faJs}
-                               description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..."}/>
-                        <Skill title={"React"}
-                               icon={faReact}
-                               description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..."}/>
-                        <Skill title={"HTML"}
-                               icon={faHtml5}
-                               description={"Labore et dolore magna aliqua Ut enim..."}/>
+            <Fade bottom>
+                <div className={style.skillsContainer}>
+                    <div className={style.skillsSecondBlock}>
+                        <Title text={'My skills'}/>
+                        <div className={style.skills}>
+                            <Skill title={"JS"}
+                                   icon={faJs}
+                                   description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..."}/>
+                            <Skill title={"React"}
+                                   icon={faReact}
+                                   description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..."}/>
+                            <Skill title={"HTML"}
+                                   icon={faHtml5}
+                                   description={"Labore et dolore magna aliqua Ut enim..."}/>
+                        </div>
                     </div>
-                </Fade>
-            </div>
+                </div>
+            </Fade>
         </div>
     );
 }
