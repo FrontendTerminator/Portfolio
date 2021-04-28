b64e2ae7f389a96d59ed7b825e711fc6ff121731
FrontendTerminator <mikutishvili.koba@gmail.com>
2021-04-28 20:01:01 +0300

    test deploy 5

--- src/burgerNavigation/BurgerNavigation.tsx
+++ src/burgerNavigation/BurgerNavigation.tsx
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
 import style from './BurgerNavigation.module.scss';
 import {Link, animateScroll as scroll} from "react-scroll";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import s from "../skills/skill/Skill.module.scss";
 
 export function BurgerNavigation() {
     const [menuIsOpen, setMenuIsOpen] = useState(false)
@@ -13,6 +16,7 @@ export function BurgerNavigation() {
         <div className={style.burgerNav}>
             <div className={menuIsOpen ? `${style.burgerNavItems} ${style.showNavItems}` : style.burgerNavItems}>
                 <Link activeClass={style.active}
+                      onClick={onButtonClick}
                       to="main"
                       spy={true}
                       smooth={true}
@@ -20,6 +24,7 @@ export function BurgerNavigation() {
                       duration={500}
                 >Main</Link>
                 <Link activeClass={style.active}
+                      onClick={onButtonClick}
                       to="skills"
                       spy={true}
                       smooth={true}
@@ -28,6 +33,7 @@ export function BurgerNavigation() {
                       className={style.link}
                 >Skills</Link>
                 <Link activeClass={style.active}
+                      onClick={onButtonClick}
                       to="projects"
                       spy={true}
                       smooth={true}
@@ -35,6 +41,7 @@ export function BurgerNavigation() {
                       duration={500}
                 >Projects</Link>
                 <Link activeClass={style.active}
+                      onClick={onButtonClick}
                       to="contacts"
                       spy={true}
                       smooth={true}
@@ -44,6 +51,10 @@ export function BurgerNavigation() {
             </div>
             <div className={style.button}
                  onClick={onButtonClick}>
+                <FontAwesomeIcon className={s.icon}
+                                 size={"1x"}
+                                 icon={faBars}
+                                 color={"#4e93e6"}/>
             </div>
         </div>
     );
