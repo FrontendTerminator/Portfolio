66bd53301bc3ed2c3c1e852a439a39c7a0edf5c4
Koba Mikutishvili <k.mikutishvili@intelliventory.ai>
2023-04-27 17:18:57 +0300

    fix

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -1,45 +1,45 @@
-import React from 'react';
-import style from './Projects.module.scss';
-import {WorkBlock} from "./work/WorkBlock";
-import {Title} from "../common/components/title/Title";
-import socialNetwork from "./../assets/images/socialNetwork.jpg"
-import todoImage from "./../assets/images/todolist.jpg"
+import React from "react";
+import style from "./Projects.module.scss";
+import { WorkBlock } from "./work/WorkBlock";
+import { Title } from "../common/components/title/Title";
+import socialNetwork from "./../assets/images/socialNetwork.jpg";
+// import todoImage from "./../assets/images/todolist.jpg"
 
-const Fade = require('react-reveal/Fade')
+const Fade = require("react-reveal/Fade");
 
 export function Projects() {
+  const social = {
+    backgroundImage: `url(${socialNetwork})`,
+  };
 
-    const social = {
-        backgroundImage: `url(${socialNetwork})`
-    }
+  // const todolist = {
+  //     backgroundImage: `url(${todoImage})`
+  // }
 
-    const todolist = {
-        backgroundImage: `url(${todoImage})`
-    }
-
-    return (
-        <div id={"projects"} className={style.worksBlock}>
-            <Fade>
-                <Title text={'Projects'}/>
-            </Fade>
-            <div className={style.worksContainer}>
-
-                <div className={style.works}>
-                    <WorkBlock
+  return (
+    <div id={"projects"} className={style.worksBlock}>
+      <Fade>
+        <Title text={"Projects"} />
+      </Fade>
+      <div className={style.worksContainer}>
+        <div className={style.works}>
+          {/* <WorkBlock
                         link={"https://FrontendTerminator.github.io/todolist"}
                         githubLink={"https://github.com/FrontendTerminator/todolist"}
                         style={todolist}
                         title={'Todolist'}
-                        description={'Todolist - react applications for keeping a list of completed tasks'}/>
-                    <WorkBlock
-                        link={"https://FrontendTerminator.github.io/way_of_the_samurai"}
-                        githubLink={"https://github.com/FrontendTerminator/way_of_the_samurai"}
-                        style={social}
-                        title={'Social network'}
-                        description={'Social network - social application'}/>
-                </div>
-            </div>
+                        description={'Todolist - react applications for keeping a list of completed tasks'}/> */}
+          <WorkBlock
+            link={"https://FrontendTerminator.github.io/way_of_the_samurai"}
+            githubLink={
+              "https://github.com/FrontendTerminator/way_of_the_samurai"
+            }
+            style={social}
+            title={"Social network"}
+            description={"Social network - social application"}
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
-
