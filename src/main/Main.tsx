e648abdde618fc992d29cf3b00a8c139baa22fc3
Koba Mikutishvili <k.mikutishvili@intelliventory.ai>
2023-04-27 17:22:25 +0300

    fix

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,63 +1,66 @@
-import React from 'react';
-import style from './Main.module.scss';
-import ava from './../assets/images/1ava.jpg';
-import Particles from 'react-particles-js';
-import ReactTypingEffect from 'react-typing-effect';
-import {BurgerNavigation} from "../burgerNavigation/BurgerNavigation";
+import React from "react";
+import style from "./Main.module.scss";
+import ava from "./../assets/images/1ava.jpg";
+import Particles from "react-particles-js";
+import ReactTypingEffect from "react-typing-effect";
 
-const Fade = require('react-reveal/Fade')
+const Fade = require("react-reveal/Fade");
 
 const particlesOpt = {
-    "particles": {
-        "number": {
-            "value": 150,
-            "density": {
-                "enable": true,
-                "value_area": 1200,
-            }
-        },
-        "color": {
-            "value": "#4E93E6"
-        },
-        "line_linked": {
-            "enable": true,
-            "color": "#4E93E6",
-            "opacity": 0.5
-        },
-        "move": {
-            "speed": 0.8
-        }
-    }
-}
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 1200,
+      },
+    },
+    color: {
+      value: "#4E93E6",
+    },
+    line_linked: {
+      enable: true,
+      color: "#4E93E6",
+      opacity: 0.5,
+    },
+    move: {
+      speed: 0.8,
+    },
+  },
+};
 
 export function Main() {
-    const photo = {
-        backgroundImage: `url(${ava})`
-    }
+  const photo = {
+    backgroundImage: `url(${ava})`,
+  };
 
-    return (
-        <div id={"main"} className={style.mainBlock}>
-            <Particles className={style.particles} params={particlesOpt}/>
-            <Fade>
-                <div className={style.container}>
-                    <div className={style.text}>
-                        <span className={style.firstString}>Hi There</span>
-                        <h1 className={style.myName}>I am
-                            <span className={style.spanName}>Jacob <span>Mikutishvili</span></span>
-                        </h1>
-                        <ReactTypingEffect className={style.secondString}
-                                           speed={100}
-                                           eraseSpeed={70}
-                                           typingDelay={2000}
-                                           text={["Front-end Developer", "Welcome to my page"]}
-                        />
-                    </div>
-                    <div className={style.photoBlock}>
-                        <div className={style.photo} style={photo}></div>
-                        <span className={style.frame}></span>
-                    </div>
-                </div>
-            </Fade>
+  return (
+    <div id={"main"} className={style.mainBlock}>
+      <Particles className={style.particles} params={particlesOpt} />
+      <Fade>
+        <div className={style.container}>
+          <div className={style.text}>
+            <span className={style.firstString}>Hi There</span>
+            <h1 className={style.myName}>
+              I am
+              <span className={style.spanName}>
+                Jacob <span>Mikutishvili</span>
+              </span>
+            </h1>
+            <ReactTypingEffect
+              className={style.secondString}
+              speed={100}
+              eraseSpeed={70}
+              typingDelay={2000}
+              text={["Front-end Developer", "Welcome to my page"]}
+            />
+          </div>
+          <div className={style.photoBlock}>
+            <div className={style.photo} style={photo}></div>
+            <span className={style.frame}></span>
+          </div>
         </div>
-    );
+      </Fade>
+    </div>
+  );
 }
